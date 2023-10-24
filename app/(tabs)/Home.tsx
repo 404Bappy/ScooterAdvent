7b523ec7b4c90d0b1f4f023784c4acc2a80699a9
Bappy71 <19103076@iubat.edu>
2023-10-24 10:59:07 +0600

    first commit

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
-export default function HomeScreen() {
+export default function DetailScreen() {
   return (
     <View style={styles.container}>
   
